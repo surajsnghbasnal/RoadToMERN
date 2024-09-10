3beb0c309f42e544e624f762561e890f1de0bf92
suraj basnal <surajsnghbasnal@gmail.com>
2024-09-10 08:59:39 +0530

    callback functions

---
+++ week 1-2 ES-6 Fundametals/2. Advance JavaScript/03_Callbacks.js
@@ -0,0 +1,19 @@
+// callback is a function passed as an argument to another function
+
+const calc = (a, b, operation) => {
+    return operation(a, b)
+}
+
+// method 1
+let addititon = calc(3, 4, (num1, num2) => num1 + num2)
+console.log(addititon);
+
+// method 2
+let subtraction = (num1, num2) => num1 - num2
+let subRes = calc(100, 90, subtraction)
+console.log(subRes)
+
+
+
+// Practically
+
