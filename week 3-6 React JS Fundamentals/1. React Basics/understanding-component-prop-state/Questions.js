bbed65ccf3868f25765f0133de7d1b99a0cee63f
suraj basnal <surajsnghbasnal@gmail.com>
2024-10-05 09:22:32 +0530

    React basics Questions

---
+++ week 3-6 React JS Fundamentals/1. React Basics/understanding-component-prop-state/Questions.js
@@ -0,0 +1,10 @@
+//? What is the difference between class-based components and functional components in React?
+
+
+
+
+
+ 	// How do you pass data between parent and child components using props?
+ 	// How does the useState hook work? Provide an example.
+ 	// Explain the role of JSX in React development.
+ 	// How do you conditionally render elements in React
