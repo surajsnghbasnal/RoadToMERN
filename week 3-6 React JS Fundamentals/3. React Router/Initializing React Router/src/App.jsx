a862a3df2fda909659ba8daeb58a97b031804dea
suraj basnal <surajsnghbasnal@gmail.com>
2024-12-17 22:01:58 +0530

    Understanding Links, NaBar, anchor Tag

--- week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/App.jsx
+++ week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Nav from './components/Nav'
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About/>} />
-          <Route path='/Contact' element={<Contact/>} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    return (
+        <div style={{ width: '100%', backgroundColor: 'salmon', display: 'flex', justifyContent: 'center', padding: '20px', gap: '20px' }}>
+
+            <p><Link to="/">Home</Link></p>
+            <p><Link to="/About">About</Link></p>
+            <p><Link to="/Contact">Contact</Link></p>
+        </div>
+    )
+}
+
+export default Nav
