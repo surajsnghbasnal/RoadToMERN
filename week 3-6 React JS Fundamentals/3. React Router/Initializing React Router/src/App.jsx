c49a3a0487ba26f2db1eff95bc59613b1ce745db
suraj basnal <surajsnghbasnal@gmail.com>
2024-12-18 19:57:16 +0530

    React router 404 Page

--- week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/App.jsx
+++ week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Nav from './components/Nav'
+import Page404 from './components/Page404'
+
 const App = () => {
   return (
     <div>
@@ -13,6 +15,7 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/About' element={<About />} />
           <Route path='/Contact' element={<Contact />} />
+          <Route path='/*' element={<Page404 />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ week 3-6 React JS Fundamentals/3. React Router/Initializing React Router/src/components/Page404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page404 = () => {
+    return (
+        <div>
+            <h1 style={{ color: 'red' }}>404 Page Not Found</h1>
+        </div>
+    )
+}
+
+export default Page404
