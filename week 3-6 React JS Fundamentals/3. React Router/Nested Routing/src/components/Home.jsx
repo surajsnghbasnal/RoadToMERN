789fd0fa59b2eeb11f46140d2c73929df057ab58
suraj basnal <surajsnghbasnal@gmail.com>
2024-12-22 20:17:26 +0530

    useLocation Hook from react router dom

--- week 3-6 React JS Fundamentals/3. React Router/Nested Routing/src/components/Home.jsx
+++ week 3-6 React JS Fundamentals/3. React Router/Nested Routing/src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Home = () => {
+    const location = useLocation()
+    console.log(location);
+
     return (
         <div>
             <h1>Home page</h1>
